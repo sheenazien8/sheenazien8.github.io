884c0d36014f7a4af59855a83b190305962477a6
sheenazien8 <sheenazien08@gmail.com>
2024-12-14 15:00:04 +0700

    add gaji-30-jt page

---
+++ src/pages/blog/gaji-30-jt.tsx
@@ -0,0 +1,17 @@
+import Head from "next/head";
+
+export default function Gaji30Jt() {
+  return (
+    <div>
+      <Head>
+        <title>Cara mendapatkan uang 30jt perbulan</title>
+        <meta name="description" content="Disini saya akan membagikan tips untuk bisa mendapatkan uang 30jt perbulan dengan usahamu sendiri" />
+        <meta property="og:title" content="Cara mendapatkan uang 30jt perbulan" />
+        <meta name="keywords" content="tips gaji 2 digit, gaji gede" />
+        <meta property="og:site_name" content="Cara mendapatkan uang 30jt perbulan" />
+        <meta property="og:locale" content="en_US" />
+      </Head>
+      <div className="text-center h-screen w-screen bg-black text-white text-5xl font-bold flex items-center justify-center">Kerjo ndes!</div>
+    </div>
+  );
+}
